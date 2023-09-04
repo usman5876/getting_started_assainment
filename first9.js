0c882544489074aad71f92469ff09afaf8b9732c
Muhammad Usman <60290215+usman5876@users.noreply.github.com>
2023-09-04 11:34:57 +0500

    5 questions solved

---
+++ first9.js
@@ -0,0 +1,34 @@
+"use strict";
+// 1- Personal Message: Store a person’s name in a variable, and print a message to that person. 
+//    Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+/*
+var persName:string = "Usman";
+console.log(`hello ${persName}, would you like learn some typescript today?`);
+*/
+// 2- Name Cases: Store a person’s name in a variable, and then print that 
+//    person’s name in lowercase, uppercase, and titlecase.
+/*
+var psname:string = "Usman";
+console.log(psname.toUpperCase());
+console.log(psname.toLowerCase());
+*/
+// 3-    Famous Quote: Find a quote from a famous person you admire. Print the quote and the 
+//       name of its author. Your output should look something like the following, including the quotation marks:
+//      Albert Einstein once said, “A person who never made a mistake never tried anything new.
+/*
+var authorName:string = "Hazarat Umar R.A";
+console.log(`${authorName} once said,"The best way to defeat someone is to beat him at politeness."`);
+*/
+//  4-  Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable 
+//      called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+/*
+var famous_person:string = "Hazarat Umar R.A";
+var message:string = "The best way to defeat someone is to beat him at politeness";
+console.log(`${famous_person} once said, "${message}."`);
+*/
+// 5- Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+//    Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace 
+//    around the name is displayed. Then print the name after striping the white spaces.
+var psname = " Usman ";
+console.log(psname);
+console.log(psname.trim());
