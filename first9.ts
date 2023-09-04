03966d1ff3cf91bbac0a78985c9154fbb21c234e
Muhammad Usman <60290215+usman5876@users.noreply.github.com>
2023-09-04 11:51:30 +0500

    fist9 is solved

--- first9.ts
+++ first9.ts
@@ -40,8 +40,33 @@ console.log(`${famous_person} once said, "${message}."`);
 // 5- Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
 //    Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace 
 //    around the name is displayed. Then print the name after striping the white spaces.
-
+/*
 var psname:string = " Usman ";
 console.log(psname);
 console.log(psname.trim());
+*/
+
+
+
+// 6- Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+//    result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//  7- 
+/*
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+*/
+
+
+
+
+// 8- Favorite Number: Store your favorite number in a variable. Then, using that variable, create a
+//    message that reveals your favorite number. Print that message.
+
+var fvrtNumber:number = 15;
+let msg: string= `My favourite number is ${fvrtNumber}`;
+console.log (msg);
+
 
